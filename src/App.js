bdc867d088ea093726d51cac76c9335e6f2d0397
Younghyun-Won <yhw1833@naver.com>
2020-08-14 14:15:21 +0900

    hoho2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Image} from 'semantic-ui-react'
+
+import Comments from './comment.js'
+
+
+import puppy from "./BBB.jpg"
+
 
 function App() {
   return (
     <div>
-    <p> 안녕하세요 ! </p>
-    <Button Primary> hi </Button>
-    <Progress percent = {33} indicating/>
+    <Image src = {puppy} centered />
+    <Comments />
+
     </div>
   );
 }
